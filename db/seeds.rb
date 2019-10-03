c648ff5d0c4ccaabeed850f0ac8fd5bc0d680fd4
hectortllo <hectortllo@gmail.com>
2019-10-03 13:15:10 -0600

    1. Puse información en el archivo seeds para crear datos en la base de datos. 2. Edité el archivo 'guia.txt' para que sepan como poder tener la información que está en el archivo seeds.rb en su propia base de datos

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+25.times do |product|
+    Product.create!(
+        name: "Product #{product}", 
+        price: rand(1.5...50.50),
+        description: "Product #{product} description"
+    )
+end
+puts "25 products created"
+
+
+5.times do |order|
+    Order.create!(
+        price: rand(15.50...150.50),
+        quantity: (order+1)
+    )
+end
+
+puts "5 orders created"
+
+5.times do |sell|
+    Sell.create!(
+        date_sell: Time.now.to_formatted_s(:date), 
+        total: rand(150.50...500.00)
+    )
+end
+
+puts "5 sells created"
