91db9f8dd0afc0d557d016cbd729aa58b34d5c8d
hectortllo <hectortllo@gmail.com>
2019-10-03 16:53:39 -0600

    Se implementaron new, create, edit y update funcionalidades en events_controller.rb

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,36 @@
 class EventsController < ApplicationController
     def index
-        @events_item = Event.all
-    end    
+        @events_items = Event.all
+    end
+
+    def new
+        @events_item = Event.new
+    end
+
+    def create
+        @events_item = Event.new(params.require(:event).permit(:name, :description))
+    
+        respond_to do |format|
+          if @events_item.save
+            format.html { redirect_to events_path, notice: 'El evento fue creado exitosamente.' }
+          else
+            format.html { render :new }
+          end
+        end
+    end
+
+    def edit
+        @events_item = Event.find(params[:id])
+    end
+
+    def update
+        @events_item = Event.find(params[:id])
+        respond_to do |format|
+          if @events_item.update(params.require(:event).permit(:name, :description))
+            format.html { redirect_to events_path, notice: 'El evento fue actualizado correctamente.' }
+          else
+            format.html { render :edit }
+          end
+        end
+    end
 end
