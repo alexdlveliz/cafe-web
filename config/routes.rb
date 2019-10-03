0a2c84ce4e254a9d3319f10bcb48054db9c604b9
hectortllo <hectortllo@gmail.com>
2019-10-03 13:40:31 -0600

    1. Se validó que al ingresar un producto, todos los campos estén llenos. 2. Se pusieron rutas personalizadas para mostrar los productos, ventas y órdenes con una ruta más entendible

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   
   get 'menu', to: 'pages#menu'
   get 'eventos', to: 'pages#event'
+  get 'product/:id', to: 'products#show', as: 'product_show'
+  get 'order/:id', to: 'orders#show', as: 'order_show'
+  get 'sell/:id', to: 'sells#show', as: 'sell_show'
 
-  resources :sells
-  resources :products
-  resources :orders
+  resources :sells, except: [:show]
+  resources :products, except: [:show]
+  resources :orders, except: [:show]
   
   root to: 'pages#home' #Se indica la página principal
 
