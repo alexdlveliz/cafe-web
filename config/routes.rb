d31ddd08f55c8c31ab2b3edf3aadb30cc6c77d2e
hectortllo <hectortllo@gmail.com>
2019-10-12 13:21:42 -0600

    Se editaron las rutas para los usuarios

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,30 @@
 Rails.application.routes.draw do
   
-  devise_for :users
-  resources :orders
-  resources :sells
-  resources :products
+  #Se le da una ruta personalizada a las rutas que trae por default 'devise'
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  
+  #Método para cambiar la ruta a un evento si se pasa de público a privado
   resources :events do 
     member do
       get :toggle_status
     end
   end
 
+  #Se personaliza la ruta para el menú y para los eventos
   get 'menu', to: 'pages#menu'
   get 'eventos', to: 'pages#event'
+
+  #Se personaliza la ruta para mostrar los eventos, ventas, órdenes
   get 'product/:id', to: 'products#show', as: 'product_show'
   get 'order/:id', to: 'orders#show', as: 'order_show'
   get 'sell/:id', to: 'sells#show', as: 'sell_show'
 
+  #Las rutas quedan como están por default, excepto para los métodos show
   resources :sells, except: [:show]
   resources :products, except: [:show]
   resources :orders, except: [:show]
   
-  root to: 'pages#home' #Se indica la página principal
+  #Se indica la página principal
+  root to: 'pages#home' 
 
 end
