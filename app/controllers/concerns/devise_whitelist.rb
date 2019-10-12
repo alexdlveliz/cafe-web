90144adda7173260eff3eeea2ace146b39f1d615
hectortllo <hectortllo@gmail.com>
2019-10-12 16:24:14 -0600

    Se agregó el concern devise_whitelist.rb para limpiar el código dentro del archivo application_controller.rb

---
+++ app/controllers/concerns/devise_whitelist.rb
@@ -0,0 +1,14 @@
+module DeviseWhitelist
+    #Este archivo está dentro de la carpeta Concerns, por eso necesitamos incluir lo siguiente
+    extend ActiveSupport::Concern
+
+    #Método para permitir el paso del parámetro 'name' al momento de crear un usario
+    included do
+        before_action :configure_permitted_parameters, if: :devise_controller?
+    end
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
+end
