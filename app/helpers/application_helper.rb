6c2974cbffcdb03afb4db4e59133bea2e4253402
hectortllo <hectortllo@gmail.com>
2019-10-14 09:37:38 -0600

    Se integró un view helper method para login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    def login_helper
+        #Condición para mostrar el login o el logout
+        if current_user.is_a?(User)
+            link_to "Logout", destroy_user_session_path, method: :delete
+        else
+            (link_to "Register", new_user_registration_path) + 
+            "<br>".html_safe + 
+            (link_to "Log in", new_user_session_path)
+        end
+    end
 end
