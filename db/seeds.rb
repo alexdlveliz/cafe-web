3d5c388c78cc6aed37b64e4c8c474facded189fb
hectortllo <hectortllo@gmail.com>
2019-10-25 11:18:20 -0600

    1. Se editó el archivo seeds.rb. 2. Se agregaron imágenes de ejemplo para los productos

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,13 +3,14 @@
     Product.create!(
         name: "Product #{product}", 
         price: rand(1.5...50.50),
-        description: "Product #{product} description"
+        description: "Product #{product} description",
+        imgProduct: "http://placehold.it/300x300"
     )
 end
 puts "25 products created"
 
-#Crear 10 eventos privados
-10.times do |event|
+#Crear 9 eventos 
+9.times do |event|
     Event.create!(
         name: "Evento #{event}",
         description: "Descripción para el evento #{event}",
@@ -20,21 +21,7 @@ puts "25 products created"
     )
 end
 
-puts "10 draft events created"
-
-#Crear 10 eventos públicos
-10.times do |event|
-    Event.create!(
-        name: "Evento público #{event}",
-        description: "Descripción para el evento #{event}",
-        main_image: "http://placehold.it/600x400",
-        thumb_image: "http://placehold.it/350x200",
-        date_event: Time.now,
-        status: 1
-    )
-end
-
-puts "10 published events created"
+puts "9 events created"
 
 #Crear 15 ventas de prueba
 15.times do |sell|
