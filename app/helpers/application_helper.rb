ec7c97d71d1ef1c60299e2ce98bd134ca72def6a
hectortllo <hectortllo@gmail.com>
2019-10-26 21:14:53 -0600

    1. Se implementó la clase 'active' según la página que se esté usando. 2. Se limpió más el código para el navbar, se creó un método helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
     #Método para mostrar el login o logout según sea el usuario que está usando la aplicación
-    def login_helper style
+    def login_helper style = ''
         if current_user.is_a?(GuestUser)
             (link_to "Register", new_user_registration_path, class: style) + 
             " ".html_safe + 
@@ -10,8 +10,52 @@ module ApplicationHelper
         end
     end
 
-    #Método helper para agregar el footer a través de la gema 'tello_view_tool
+    #Método helper para agregar el footer a través de la gema 'tello_view_tool'
     def copyright_generator
         TelloViewTool::Renderer.copyright "MultiTorial", "Todos los derechos reservados"
+    end
+
+    #Hash para el navbar. Este hash se va a iterar en el método 'nav_helper' para colocdar en pantalla
+    #todas las rutas existentes
+    def nav_items
+        [
+            {
+                url: root_path,
+                title: 'Home'
+            },
+            {
+                url: products_path,
+                title: 'Menú'
+            },
+            {
+                url: events_path,
+                title: 'Eventos'
+            },
+            {
+                url: sells_path,
+                title: 'Venta'
+            },
+            {
+                url: orders_path,
+                title: 'Órdenes'
+            },
+        ]
+    end
+
+    #Método para el navbar.
+        #style es para los estilos en CSS
+        #tag_type es para la etiqueta de HTML
+    def nav_helper style, tag_type
+        nav_links = ''
+        nav_items.each do |item|
+            nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+        end
+        nav_links.html_safe
+    end
+
+    #Método para agregar la clase 'active' al css
+    def active? path 
+        #Retornar 'active' si la current_path es igual al parámetro 'path'
+        "active" if current_page? path 
     end
 end
