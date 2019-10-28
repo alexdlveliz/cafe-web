e778035b676e16bb289087b414edc959cca7ca5f
hectortllo <hectortllo@gmail.com>
2019-10-27 23:18:31 -0600

    Se terminó de implementar el manejo de las imágenes de los produtos con conexión a AWS, aún falta arreglar algunos errores

--- app/uploaders/product_uploader.rb
+++ app/uploaders/product_uploader.rb
@@ -1,7 +1,7 @@
 #Clase creada al ejecutar rails g uploader Product
 class ProductUploader < CarrierWave::Uploader::Base
   #Lugar donde se van a almacenar las imágenes
-  storage :file
+  storage :aws
 
   #Directorio donde se van a guardar las imágenes
   def store_dir
