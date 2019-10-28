8aaaae4bd388901c8998275d136136c8d421d79b
hectortllo <hectortllo@gmail.com>
2019-10-27 23:41:56 -0600

    Se arreglaron los errores que había con las imágenes de los productos conectados con AWS

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,13 @@
 module ProductsHelper
+    def image_generator(height:, width:)
+        "http://placehold.it/#{height}x#{width}"
+    end
+
+    def product_img img, type
+        if img.model.imgProduct?
+            img
+        elsif type == 'imgProduct'
+            image_generator(height: '300', width: '300')
+        end
+    end
 end
