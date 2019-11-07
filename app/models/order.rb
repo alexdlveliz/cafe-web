9edc9c746044eefe050570a03930c8da8b840d00
hectortllo <hectortllo@gmail.com>
2019-11-07 00:04:40 -0600

    Intento de arreglar error con heroku

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,8 @@
 #Este modelo (tabla) tiene una relación muchos a muchos con product
 class Order < ApplicationRecord
   #Relaciones con las demás tablas. 
-  belongs_to :product, optional: true
-  belongs_to :sell, optional: true
+  belongs_to :product
+  belongs_to :sell
 
   
 end
